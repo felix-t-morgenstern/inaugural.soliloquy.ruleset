bb864db5000d1bda0e0772f69fef536e47a02a35
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2022-10-08 00:52:09 -0400

    feat: Define, test, and implement ElementFactory
    NB: I'm using a permissive API key at the moment; not advisable for the long-term, but sticking with no real security for now

---
+++ src/main/java/inaugural/soliloquy/ruleset/entities/factories/ElementFactoryImpl.java
@@ -0,0 +1,64 @@
+package inaugural.soliloquy.ruleset.entities.factories;
+
+import inaugural.soliloquy.tools.Check;
+import soliloquy.specs.graphics.assets.ImageAssetSet;
+import soliloquy.specs.ruleset.definitions.ElementDefinition;
+import soliloquy.specs.ruleset.entities.Element;
+import soliloquy.specs.ruleset.entities.factories.ElementFactory;
+
+import java.util.function.Function;
+
+public class ElementFactoryImpl implements ElementFactory {
+    private final Function<String, ImageAssetSet> GET_IMAGE_ASSET_SET;
+
+    public ElementFactoryImpl(Function<String, ImageAssetSet> getImageAssetSet) {
+        GET_IMAGE_ASSET_SET = Check.ifNull(getImageAssetSet, "getImageAssetSet");
+    }
+
+    @Override
+    public Element make(ElementDefinition definition) throws IllegalArgumentException {
+        Check.ifNull(definition, "definition");
+        Check.ifNullOrEmpty(definition.id, "definition.id");
+        Check.ifNullOrEmpty(definition.name, "definition.name");
+        Check.ifNullOrEmpty(definition.imageAssetSetId, "definition.imageAssetSetId");
+        ImageAssetSet imageAssetSet = GET_IMAGE_ASSET_SET.apply(definition.imageAssetSetId);
+        if (imageAssetSet == null) {
+            throw new IllegalArgumentException("ElementFactoryImpl.make: definition" +
+                    ".imageAssetSetId does not correspond to a valid ImageAssetSet");
+        }
+        
+        return new Element() {
+            private String name = definition.name;
+
+            @Override
+            public String id() throws IllegalStateException {
+                return definition.id;
+            }
+
+            @Override
+            public String getName() {
+                return name;
+            }
+
+            @Override
+            public void setName(String name) {
+                this.name = Check.ifNullOrEmpty(name, "name");
+            }
+
+            @Override
+            public ImageAssetSet imageAssetSet() {
+                return imageAssetSet;
+            }
+
+            @Override
+            public String getInterfaceName() {
+                return Element.class.getCanonicalName();
+            }
+        };
+    }
+
+    @Override
+    public String getInterfaceName() {
+        return ElementFactory.class.getCanonicalName();
+    }
+}
---
+++ src/test/java/inaugural/soliloquy/ruleset/entities/factories/ElementFactoryImplTests.java
@@ -0,0 +1,95 @@
+package inaugural.soliloquy.ruleset.entities.factories;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import soliloquy.specs.graphics.assets.ImageAssetSet;
+import soliloquy.specs.ruleset.definitions.ElementDefinition;
+import soliloquy.specs.ruleset.entities.Element;
+import soliloquy.specs.ruleset.entities.factories.ElementFactory;
+
+import java.util.HashMap;
+
+import static inaugural.soliloquy.tools.random.Random.randomString;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+class ElementFactoryImplTests {
+    private final String ID = randomString();
+    private final String NAME = randomString();
+    private final String IMAGE_ASSET_SET_ID = randomString();
+    private final HashMap<String, ImageAssetSet> IMAGE_ASSET_SETS = new HashMap<>();
+
+    @Mock
+    private ImageAssetSet mockImageAssetSet;
+
+    private ElementFactory elementFactory;
+
+    @BeforeEach
+    void setUp() {
+        mockImageAssetSet = mock(ImageAssetSet.class);
+        IMAGE_ASSET_SETS.put(IMAGE_ASSET_SET_ID, mockImageAssetSet);
+
+        elementFactory = new ElementFactoryImpl(IMAGE_ASSET_SETS::get);
+    }
+
+    @Test
+    void testConstructorWithInvalidParams() {
+        assertThrows(IllegalArgumentException.class, () -> new ElementFactoryImpl(null));
+    }
+
+    @Test
+    void testMake() {
+        Element element = elementFactory.make(new ElementDefinition(ID, NAME, IMAGE_ASSET_SET_ID));
+
+        assertNotNull(element);
+        assertEquals(ID, element.id());
+        assertEquals(NAME, element.getName());
+        assertSame(mockImageAssetSet, element.imageAssetSet());
+        assertEquals(Element.class.getCanonicalName(), element.getInterfaceName());
+    }
+
+    @Test
+    void testMakeWithInvalidParams() {
+        assertThrows(IllegalArgumentException.class, () -> elementFactory.make(null));
+        assertThrows(IllegalArgumentException.class,
+                () -> elementFactory.make(new ElementDefinition(null, NAME, IMAGE_ASSET_SET_ID)));
+        assertThrows(IllegalArgumentException.class,
+                () -> elementFactory.make(new ElementDefinition("", NAME, IMAGE_ASSET_SET_ID)));
+        assertThrows(IllegalArgumentException.class,
+                () -> elementFactory.make(new ElementDefinition(ID, null, IMAGE_ASSET_SET_ID)));
+        assertThrows(IllegalArgumentException.class,
+                () -> elementFactory.make(new ElementDefinition(ID, "", IMAGE_ASSET_SET_ID)));
+        assertThrows(IllegalArgumentException.class,
+                () -> elementFactory.make(new ElementDefinition(ID, NAME, null)));
+        assertThrows(IllegalArgumentException.class,
+                () -> elementFactory.make(new ElementDefinition(ID, NAME, "")));
+        assertThrows(IllegalArgumentException.class,
+                () -> elementFactory.make(new ElementDefinition(ID, NAME, "not a valid " +
+                        "imageAssetSetId")));
+    }
+
+    @Test
+    void testSetNameOnCreatedElement() {
+        Element element = elementFactory.make(new ElementDefinition(ID, NAME, IMAGE_ASSET_SET_ID));
+        String newName = randomString();
+
+        element.setName(newName);
+
+        assertEquals(newName, element.getName());
+    }
+
+    @Test
+    void testSetNameOnCreatedElementWithInvalidParams() {
+        Element element = elementFactory.make(new ElementDefinition(ID, NAME, IMAGE_ASSET_SET_ID));
+
+        assertThrows(IllegalArgumentException.class, () -> element.setName(null));
+        assertThrows(IllegalArgumentException.class, () -> element.setName(""));
+    }
+
+    @Test
+    void testGetInterfaceName() {
+        assertEquals(ElementFactory.class.getCanonicalName(), elementFactory.getInterfaceName());
+    }
+}
